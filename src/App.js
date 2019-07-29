8bdf580007d96c8a1a7d69e8e15229446a77ede1
Christian Yores <christianyores@gmail.com>
2019-07-29 14:12:18 -0400

    Biolerplate css, Button component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Components
+import Button from "./components/Button";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function handleClick() {
+        alert('clicked');
+    }
+
+    return (
+        <Button type="button" text="primary button" onPress={handleClick}/>
+    );
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    margin: 0.5rem;
+`
+
+class Button extends Component {
+    render() { 
+        return ( 
+            <StyledButton 
+                type={this.props.type}
+                onClick={this.props.onPress}
+                disabled={this.props.isDisabled}>
+                {this.props.text}
+            </StyledButton>
+         );
+    }
+}
+ 
+export default Button;
